0c78f88340e2a16a0da76aaf7437fd57a5ab73a7
Robin Freyler <robin.freyler@gmail.com>
2023-01-10 17:01:26 +0100

    remove main and add a test

---
+++ tests/bytecode.rs
@@ -0,0 +1,98 @@
+pub use union_fn::union_fn;
+pub use union_fn::CallWithContext as _;
+
+#[union_fn]
+trait Instruction {
+    type Context = ExecutionContext;
+    type Output = Result<Control, TrapCode>;
+
+    fn constant(ctx: &mut Self::Context, value: u64) -> Self::Output {
+        ctx.push(value);
+        ctx.next_instr()
+    }
+
+    fn dup(ctx: &mut Self::Context, depth: usize) -> Self::Output {
+        let dup = ctx.nth(depth)?;
+        ctx.push(dup);
+        ctx.next_instr()
+    }
+
+    fn i64_add(ctx: &mut Self::Context) -> Self::Output {
+        let rhs = ctx.pop()? as i64;
+        let lhs = ctx.pop()? as i64;
+        let result = lhs.wrapping_add(rhs);
+        ctx.push(result as u64);
+        ctx.next_instr()
+    }
+
+    fn i64_div(ctx: &mut Self::Context) -> Self::Output {
+        let rhs = ctx.pop()? as i64;
+        if rhs == 0 {
+            return Err(TrapCode::DivisionByZero);
+        }
+        let lhs = ctx.pop()? as i64;
+        let result = match lhs.overflowing_div(rhs) {
+            (result, false) => Ok(result),
+            _ => Err(TrapCode::IntegerOverflow),
+        }?;
+        ctx.push(result as u64);
+        ctx.next_instr()
+    }
+
+    fn ret(ctx: &mut Self::Context) -> Self::Output {
+        let _ = ctx; // TODO: make it possible to write _ctx
+        Ok(Control::Return)
+    }
+}
+
+pub struct ExecutionContext {
+    instrs: Vec<Instruction>,
+    ip: usize,
+    stack: Vec<u64>,
+}
+
+impl ExecutionContext {
+    pub fn exec_instr(&mut self) -> Result<Control, TrapCode> {
+        self.instrs
+            .get(self.ip)
+            .copied()
+            .ok_or_else(|| TrapCode::UnreachableCodeReached)?
+            .call(self)
+    }
+
+    pub fn next_instr(&mut self) -> Result<Control, TrapCode> {
+        self.ip += 1;
+        Ok(Control::Continue)
+    }
+
+    pub fn pop(&mut self) -> Result<u64, TrapCode> {
+        self.stack.pop().ok_or_else(|| TrapCode::StackUnderflow)
+    }
+
+    pub fn push(&mut self, value: u64) {
+        self.stack.push(value)
+    }
+
+    pub fn nth(&self, depth: usize) -> Result<u64, TrapCode> {
+        self.stack
+            .iter()
+            .rev()
+            .nth(depth)
+            .copied()
+            .ok_or_else(|| TrapCode::StackUnderflow)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Control {
+    Continue,
+    Return,
+}
+
+#[derive(Copy, Clone)]
+pub enum TrapCode {
+    UnreachableCodeReached,
+    IntegerOverflow,
+    DivisionByZero,
+    StackUnderflow,
+}
