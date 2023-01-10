62c33c91d0d1b3550779298652a9cacc715e18c2
Robin Freyler <robin.freyler@gmail.com>
2023-01-10 20:10:43 +0100

    add more UI tests

---
+++ tests/ui/fail/fn-invalid-context-03.rs
@@ -0,0 +1,8 @@
+fn main() {}
+
+#[union_fn::union_fn]
+trait Foo {
+    type Context = i32;
+
+    fn foo(ctx: &mut i32) {}
+}
---
+++ tests/ui/fail/fn-missing-context-02.rs
@@ -0,0 +1,9 @@
+fn main() {}
+
+#[union_fn::union_fn]
+trait Foo {
+    type Context = i32;
+
+    fn foo(ctx: &mut Self::Context) {}
+    fn bar() {}
+}
